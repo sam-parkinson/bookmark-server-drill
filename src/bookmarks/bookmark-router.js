d67ef20388649a54f7483b651820e20de8a5abe8
Sam Parkinson <parkinsonsp42@gmail.com>
2019-06-08 21:53:16 -0400

    Finish drill

---
+++ src/bookmarks/bookmark-router.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const uuid = require('uuid/v4');
+const logger = require('../logger');
+const bookmarks = require('..store');
+
+const bookmarkRouter = express.Router();
+const bodyParser = express.json();
+
+bookmarkRouter
+  .route('/bookmarks')
+  .get((req, res) => {
+    res.json(bookmarks);
+  })
+  .post(bodyParser, (req, res) => {
+    const { title, url, description, rating } = req.body;
+
+    if (!title || !url || !description || !rating ) {
+      logger.error(`Incomplete bookmark provided`);
+      return res
+        .status(400)
+        .send('Invalid data');
+    }
+
+    const id = uuid();
+
+    const bookmark = {
+      id,
+      title,
+      url,
+      description,
+      rating
+    };
+
+    bookmarks.push(bookmark);
+
+    logger.info(`Bookmark with id ${id} created`);
+
+    res
+      .status(201)
+      .location(`http://localhost:8000/card/${id}`)
+      .json(bookmark);
+  });
+
+bookmarkRouter
+  .route('/bookmarks/:id')
+  .get((req, res) => {
+    const { id } = req.params;
+    const bookmark = bookmarks.find(bk => bk.id == id);
+
+    if (!bookmark) {
+      logger.error(`Bookmark with id ${id} not found.`);
+      return res
+        .status(404)
+        .send('Bookmark not found');
+    }
+
+    res.json(list);
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+
+    const bkIndex = bookmarks.findIndex(bk => bk.id == id);
+
+    if (bkIndex === -1) {
+      logger.error(`Bookmark with id ${id} not found.`);
+      return res
+        .status(404)
+        .send('Not found');
+    }
+
+    bookmarks.splice(bkIndex, 1);
+
+    logger.info(`Bookmark with id ${id} deleted.`);
+    res
+      .status(204)
+      .end();
+  });
+
+module.exports = bookmarkRouter;
---
+++ src/store.js
@@ -0,0 +1,9 @@
+const bookmarks = [{
+  id: 1,
+  title: 'Giant in the Playground',
+  url: 'https://giantitp.com/',
+  description: 'The Order of the Stick',
+  rating: 5
+}];
+
+module.exports = bookmarks;
