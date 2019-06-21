15da6b0f5a028dd343ccc891a6c7761787862616
Sam Parkinson <parkinsonsp42@gmail.com>
2019-06-21 22:07:51 +0000

    add seed script

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,12 @@
+insert into bookmarks (title, url, description, rating)
+values
+  ('Giant in the Playground', 'http://giantitp.com', 'The Order of the Stick', 5),
+  ('Homestuck', 'http://homestuck.com', 'Homestuck', 5),
+  ('Ultimate Guitar', 'http://ultimate-guitar.com', 'Guitar tabs', 4),
+  ('Wikipedia', 'http://en.wikipedia.org', 'English language Wikipedia', 4),
+  ('Pathfinder SRD', 'http://d20pfsrd.com', 'Pathfinder classes, monsters, items, & miscellany', 5),
+  ('5E SRD', 'http://5e.d20srd.com', 'Dungeons and Dragons 5th Edition SRD', 4),
+  ('MDN', 'https://developer.mozilla.org/en-US/', 'Mozilla Developere Network docs', 5),
+  ('GitHub', 'https://github.com', 'Online Git Repositories', 5),
+  ('Reddit', 'https://reddit.com', 'Internet hivemind', 3),
+  ('Twitter', 'https://twitter.com', 'Microblogging platform', 3);  
